63ad02427be9fc676f1fb3e6d0f2bfcfd8065497
apshora427 <khanmodhu96@gmail.com>
2025-04-07 17:11:31 +0600

    Describe the changes you made

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,19 +3,22 @@ import ContactFrom from './ContactFrom'
 
 const ContactCard = () => {
     return (
-        <section className='bg-dark-background-color pt-[130px] '>
-            <div className="container">
-                <div className='grid grid-cols-1 gap-[80px]'>
-                    <div className='w-[335px] mx-auto'>
-                        <h1 className='font-bold text-[40px] leading-[57px] text-white '>We Have Branches All Over The World</h1>
-                        <p className='font-medium text-[14px] leading-[20px] text-white mt-[15px] '>The gradual accumulation of information about atomic and small-scale behaviour during the first quarter of the 20th century, which gave some indications about how small things do behave, produced an increasing confusion which was Heisenberg, and Born.</p>
+        <section className='bg-dark-background-color pt-[130px] relative lg:h-[900px]'>
+            <div className="lg:container">
+                <div className='grid grid-cols-1 md:grid-cols-[auto_3fr] gap-[80px] md:gap-0'>
+                    <div className='w-[335px] md:w-[400px] lg:w-[476px] mx-auto pl-[40px]'>
+                        <h1 className='font-bold text-[40px] md:text-[35px] lg:text-[40px] leading-[57px] text-white '>We Have Branches All Over The World</h1>
+                        <p className='font-medium text-[14px] md:text-[12px] lg:text-[14px] leading-[20px] text-white mt-[15px] '>The gradual accumulation of information about atomic and small-scale behaviour during the first quarter of the 20th century, which gave some indications about how small things do behave, produced an increasing confusion which was Heisenberg, and Born.</p>
                     </div>
                     <div>
-                        <img className='w-[400px] mx-auto' src={map} alt="" />
+                        <img className='w-[400px] md:mt-[60px] mx-auto' src={map} alt="" />
                     </div>
                 </div>
+                <div className=''>
+                    <ContactFrom />
+                </div>
             </div>
-            <ContactFrom/>
+           
         </section>
     )
 }
--- src/components/ContactFrom.jsx
+++ src/components/ContactFrom.jsx
@@ -3,11 +3,13 @@ import arrow from '../assets/arrow.png'
 
 function ContactFrom() {
   return (
-    <section className='mt-[110px] bg-[#fff] pb-[40px]'>
+    <section className='mt-[110px] bg-[#fff] pb-[40px] md:pb-0 '>
+      
+      <div className=''>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[auto_5fr] md:gap-[0px]'>
       <div>
-        <img className='w-[900px] h-[260px] object-cover' src={contact_image} alt="" />
+        <img className='w-[900px] md:w-[700px] lg:w-[400px] h-[260px] md:h-[700px] object-cover' src={contact_image} alt="" />
       </div>
-      <div className='container'>
         <div className='flex justify-center'>
           <form className='' action="">
           <h2 className='font-bold text-[40px] leading-[57px] text-text-color w-[243px] h-[114px] my-[40px]'>Get A Free Quote Here</h2>
@@ -21,7 +23,7 @@ function ContactFrom() {
             <input
               type="text"
               placeholder='Full Name'
-              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
+              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] md:w-[360px] lg:w-[525px] py-[11px] px-[21px]  border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
             /> 
             <br />
             <label
@@ -34,7 +36,7 @@ function ContactFrom() {
             <input
               type="text"
               placeholder='example@gmail.com'
-              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
+              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] md:w-[360px] lg:w-[525px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
             /> 
             <br />
             <label
@@ -47,7 +49,7 @@ function ContactFrom() {
             <input
               type="text"
               placeholder='Please Select'
-              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
+              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] md:w-[360px] lg:w-[525px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
             /> 
             <br />
             <label
@@ -60,15 +62,16 @@ function ContactFrom() {
             <input
               type="text"
               placeholder='4:00 Available'
-              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
+              className='bg-[#f9f9f9] text-[14px] leading-[28px] font-normal text-second-text-color w-[311px] md:w-[360px] lg:w-[525px] py-[11px] px-[21px] border-[2px] border-dark-muted-color rounded-[5px] mt-[10px] mb-[5px]'
             />
             <br />
             <br />
-            <button className='bg-primary-color text-white px-[85px] py-[15px] rounded-[5px]'>
+            <button className='bg-primary-color text-white px-[85px] md:px-[110px] lg:px-[190px] py-[15px] rounded-[5px]'>
               <a className='font-bold text-[14px] leading-[28px]' href="#">Book Appointment</a>
             </button>
           </form>
         </div>
+      </div>
       </div>
     </section>
   )
